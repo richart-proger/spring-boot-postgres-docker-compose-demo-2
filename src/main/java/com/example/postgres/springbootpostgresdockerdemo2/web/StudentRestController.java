bc56f848dbf4fbd032cd530882d288b790f79970
Artur Galimov <satorica@mail.ru>
2022-04-15 17:54:43 +0300

    add swagger

--- src/main/java/com/example/postgres/springbootpostgresdockerdemo2/web/StudentRestController.java
+++ src/main/java/com/example/postgres/springbootpostgresdockerdemo2/web/StudentRestController.java
@@ -2,6 +2,7 @@ package com.example.postgres.springbootpostgresdockerdemo2.web;
 
 import com.example.postgres.springbootpostgresdockerdemo2.model.Student;
 import com.example.postgres.springbootpostgresdockerdemo2.service.StudentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,16 +23,19 @@ public class StudentRestController {
     }
 
     @PostMapping(path = "item")
+    @ResponseStatus(HttpStatus.CREATED)
     public void add(@RequestBody Student student) {
         studentService.add(student);
     }
 
     @DeleteMapping(path = "item/{studentId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long studentId) {
         studentService.delete(studentId);
     }
 
     @PutMapping(path = "item")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody Student student) {
         studentService.update(student);
     }
